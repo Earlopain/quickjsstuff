074243791a30613462741f242a91ae6f5cf769f7
Earlopain <earlopain@c5h8no4na.net>
2019-09-11 21:11:34 +0200

    Modified to new parser structure

--- comicdownloader/parser/custom/lackadaisy.js
+++ comicdownloader/parser/custom/lackadaisy.js
@@ -1,40 +1,32 @@
 const ParserBase = require("../parserBase");
 
-class ParserLackADaisy extends ParserBase {
-    constructor(options){
+class Parser extends ParserBase {
+    constructor(options) {
         super(options);
+        this.cssSelectorNext = "div.comicnav:nth-child(4) > div:nth-child(3) > a:nth-child(1)";
+        this.cssSelectorPrevious = "div.comicnav:nth-child(4) > div:nth-child(1) > a:nth-child(1)";
+        this.cssSelectorImage = "#content > img:nth-child(2)";
+        this.mainPage = "https://lackadaisycats.com/";
     }
 
-    getStartURL(){
+    getStartURL() {
         return "https://lackadaisycats.com/comic.php?comicid=1";
     }
 
-    getCurrentURL(){
+    getCurrentURL() {
         return "https://lackadaisycats.com/comic.php";
     }
 
-    getNextPageURL(){
-        return "https://lackadaisycats.com" + this.document.getElementsByClassName("next")[0].children[0].href;
+    noNextPage() {
+        return this.getNextPageURL() === null;
     }
 
-    getPreviousPageURL(){
-        return "https://lackadaisycats.com" + this.document.getElementsByClassName("prev")[0].children[0].href;
-    }
-
-    noNextPage(){
-        return this.document.getElementsByClassName("next")[0].children.length === 0;
-    }
-
-    noPreviousPage(){
-        return this.document.getElementsByClassName("prev")[0].children.length === 0;
-    }
-
-    getImageURL(){
-        return "https://lackadaisycats.com/" + this.document.getElementById("content").children[1].src;
+    noPreviousPage() {
+        return this.getPreviousPageURL === null;
     }
 }
 
-ParserLackADaisy.fullName = "LackADaisy";
-ParserLackADaisy.shortName = "lackadaisy";
+Parser.fullName = "LackADaisy";
+Parser.shortName = "lackadaisy";
 
-module.exports = ParserLackADaisy;
+module.exports = Parser;
