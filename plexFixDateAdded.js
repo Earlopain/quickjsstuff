9d875e35e83c2adf43fcd068565915788eace805
Earlopain <earlopain@c5h8no4na.net>
2019-10-27 22:18:01 +0100

    Do it for all files in folder + added logging

--- plexFixDateAdded.js
+++ plexFixDateAdded.js
@@ -1,75 +1,83 @@
 const fs = require("fs");
+const glob = require("glob");
 const sqlite = require("better-sqlite3");
 
-const dbFilePath = "/media/earlopain/External/Programmieren/quickjsstuff/com.plexapp.plugins.library.db";
-const mountPath = "/media/earlopain/plex";
-const folders = ["plexmedia/Pictures/e621", "plexmedia/Pictures/e621animation", "plexmedia/Pictures/e621safe"];
-const librarySections = ["e621", "e621safe", "e621animation"];
-
+const dbFilePath = "/media/plex/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db";
+const path = "/media/plex/plexmedia/e621";
+console.log("Collecting local files");
+const localFiles = glob.sync(path + "/**/*", { nodir: true });
 const allFiles = {};
 
+const db = sqlite(dbFilePath, { fileMustExist: true });
+
+const mediaParts = db.prepare('select * from media_parts').all();
+const mediaItems = db.prepare('select * from media_items').all();
+const metadataItems = db.prepare("select * from metadata_items").all();
 //populate local file information. key is the filename
 //it also gets the filepath and a datestring at which it was created
 //this datestring will be written as is into the db
-for (let folder of folders) {
-    folder = mountPath + "/" + folder;
-    const files = fs.readdirSync(folder);
-    for (const file of files) {
-        const filePath = folder + "/" + file;
-        let properties = {};
-        properties["filepath"] = filePath;
-        const stats = fs.statSync(filePath);
-        const date = new Date(stats.mtimeMs);
-        const dateString = createDBDate(date);
-        properties["created"] = dateString;
-        allFiles[file.split(".")[0]] = properties;
-    }
-}
+console.log("Getting local file dates. This might take a while");
+for (let mediaPart of mediaParts) {
+    if (!localFiles.includes(mediaPart.file))
+        continue;
+    let mediaItem = mediaItems.filter(element => {
+        return element.id === mediaPart.media_item_id;
+    })[0];
+    let metadataItem = metadataItems.filter(element => {
+        return element.id === mediaItem.metadata_item_id;
+    })[0];
 
-const db = sqlite(dbFilePath, { fileMustExist: true });
-//get all sectionIds where the name is in librarySections
-//we do not want to operate in all sections
-const sectionIDs = db.prepare('select id from library_sections where name in ("' + librarySections.join('","') + '")').all().map(e => e.id);
-//get all items from those sections which did not get deleted
-const allDBFiles = db.prepare('SELECT id, library_section_id, title, added_at FROM metadata_items WHERE deleted_at is NULL AND library_section_id in (' + sectionIDs.join(",") + ');').all()
-//update a items date information
-const preparedStatement = db.prepare("UPDATE metadata_items SET added_at = ?, originally_available_at = ?, created_at = ? WHERE id = ?");
+    let properties = {};
+    const stats = fs.statSync(mediaPart.file);
+    const date = new Date(stats.mtimeMs);
+    const dateString = createDBDate(date);
+    properties["created"] = dateString;
+    properties["metadataItem"] = metadataItem;
+    allFiles[mediaPart.file] = properties;
+}
 
 const clusterings = {};
 let itemToCluster = {};
-for (const id of sectionIDs) {
-    clusterings[id] = {};
-}
 
 db.exec("BEGIN");
+console.log("Fixing file dates");
 updateFileDates();
+console.log("Removing clusters");
 removeItemClusers();
+console.log("Generating new clusters");
 generateNewClusters();
+console.log("Adding files to clusters");
 addItemsToCluster();
 db.exec("COMMIT");
+console.log("COMMITED");
 
-function updateFileDates(){
-    for (const dbFile of allDBFiles) {
-        const dbTile = dbFile.title;
-        const localFile = allFiles[dbTile];
-        const dbDateFull = dbFile.added_at;
-        const localDateSplit = localFile.created.split(" ")[0];
+function updateFileDates() {
+    //update a items date information
+    const preparedStatement = db.prepare("UPDATE metadata_items SET added_at = ?, originally_available_at = ?, created_at = ? WHERE id = ?");
+    for (const key of Object.keys(allFiles)) {
+        const dbFile = allFiles[key];
+        const dbDateFull = dbFile.metadataItem.added_at;
+        const localDateSplit = dbFile.created.split(" ")[0];
+        if (clusterings[dbFile.metadataItem.library_section_id] === undefined) {
+            clusterings[dbFile.metadataItem.library_section_id] = {};
+        }
         //store items of section seperatly and also seperate section entries by the date it was added
         //this is important so we can later reconstruct the clusters in which they will be display (timeline view)
-        if(clusterings[dbFile.library_section_id][localDateSplit] === undefined){
-            clusterings[dbFile.library_section_id][localDateSplit] = [];
+        if (clusterings[dbFile.metadataItem.library_section_id][localDateSplit] === undefined) {
+            clusterings[dbFile.metadataItem.library_section_id][localDateSplit] = [];
         }
-        clusterings[dbFile.library_section_id][localDateSplit].push(dbFile.id);
-    
-        if (dbDateFull !== localFile.created) {
-            preparedStatement.run(localFile.created, localFile.created, localFile.created, dbFile.id.toString());
+        clusterings[dbFile.metadataItem.library_section_id][localDateSplit].push(dbFile.metadataItem.id);
+
+        if (dbDateFull !== dbFile.created) {
+            preparedStatement.run(dbFile.created, dbFile.created, dbFile.created, dbFile.metadataItem.id.toString());
         }
     }
 }
 
 function removeItemClusers() {
     //simply remove all clusters of those sections, since we will rebuild them anyways
-    let preparedStatement = db.prepare("DELETE FROM metadata_item_clusters where library_section_id in (" + sectionIDs.join(",") + ")");
+    //TODO still deleted all clusters for id, only delete if sure that no items will remain
+    let preparedStatement = db.prepare("DELETE FROM metadata_item_clusters where library_section_id in (" + Object.keys(clusterings).join(",") + ")");
     preparedStatement.run();
 }
 
